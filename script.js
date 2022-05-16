589bee1b8be5e0f8e019ce3347211c5ad70b01ed
pavsoldatov <snowschoe@gmail.com>
2022-05-17 00:11:19 +0300

    Generate grid with proper borders

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-let gridSizeElement = document.querySelector(".settings__grid-size");
-let label = document.querySelector(".settings > label");
+const gridSizeElement = document.querySelector(".settings__grid-size-input");
+const sketchGrid = document.querySelector(".sketch-grid");
+const gridSizeInput = document.querySelector(".settings__grid-size-input");
+const gridSizeLabel = document.querySelector(".settings__grid-label");
+const gridSquare = document.createElement("div");
+const defaultGridSize = Number(
+  document.querySelector(".settings__grid-size-input").value
+);
 
+function addCSS(element, style) {
+  for (let property in style) element.style[property] = style[property];
+}
+
+// adds css for default grid appearing on load
+addCSS(sketchGrid, {
+  gridTemplateRows: `repeat(${gridSizeInput.value}, 1fr)`,
+  gridTemplateColumns: `repeat(${gridSizeInput.value}, 1fr)`,
+});
+
+// populate squares across the grid and draw borders
+function generateGrid(grid, square, size) {
+  const children = grid.children;
+  const borderStyle = "1px solid #555";
+
+  for (let i = 1; i <= size * size; i++) {
+    square = document.createElement("div");
+    grid.append(square);
+    square.style.cssText = `border-left: ${borderStyle}; border-top: ${borderStyle};`;
+  } // borders for every last element in a row
+  for (let i = 1; i <= size * size; i += size) {
+    children[i + size - 2].style.cssText += `border-right: ${borderStyle}`;
+  } // borders only for elements in the last row
+  for (let i = children.length - 1; i >= children.length - size; i--) {
+    children[i].style.cssText += `border-bottom: ${borderStyle}`;
+  }
+}
+
+// function call so it populates squares into the grid on page load
+generateGrid(sketchGrid, gridSquare, defaultGridSize);
+
+// update grid size on input change
 gridSizeElement.addEventListener("change", (e) => {
-  let gridSizeValue = e.target.value;
+  const gridSizeValue = Number(e.target.value);
 
   addCSS(sketchGrid, {
     gridTemplateRows: `repeat(${gridSizeValue}, 1fr)`,
     gridTemplateColumns: `repeat(${gridSizeValue}, 1fr)`,
   });
-  label.innerText = `${gridSizeValue} x ${gridSizeValue}`
-  sketchGrid.append(square);
 
-  console.log(gridSizeValue);
-});
-
-function addCSS(element, style) {
-  for (let property in style) {
-    console.log(style);
-    element.style[property] = style[property];
-  }
-}
-
-let square = document.createElement("div");
-addCSS(square, {
-  border: `1px #222 solid`,
-  backgroundColor: `lightblue`,
-});
-
-let sketchGrid = document.querySelector(".sketch-grid");
-addCSS(sketchGrid, {
-  boxShadow: `0 0 8px 8px #9f9f9f`,
-  border: `2px red solid`,
-  width: "512px",
-  height: "512px",
-  display: "grid",
+  gridSizeLabel.innerText = `${gridSizeValue} x ${gridSizeValue}`;
+  sketchGrid.innerHTML = "";
+  generateGrid(sketchGrid, gridSquare, gridSizeValue);
 });
