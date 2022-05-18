2c4f7660dcbafcf5f8232de53d5269d4cf0973fd
pavsoldatov <snowschoe@gmail.com>
2022-05-18 17:50:39 +0300

    Add a working grid toggle button

--- script.js
+++ script.js
@@ -1,53 +1,83 @@
-const gridSizeElement = document.querySelector(".settings__grid-size-input");
 const sketchGrid = document.querySelector(".sketch-grid");
 const gridSizeInput = document.querySelector(".settings__grid-size-input");
 const gridSizeLabel = document.querySelector(".settings__grid-label");
 const gridSquare = document.createElement("div");
-const defaultGridSize = Number(
+const toggleGridButton = document.querySelector(
+  ".settings__toggle-grid-button"
+);
+let gridSize = Number(
   document.querySelector(".settings__grid-size-input").value
 );
+let borderStyle = "1px solid #555";
+
 
 function addCSS(element, style) {
   for (let property in style) element.style[property] = style[property];
 }
 
-// adds css for default grid appearing on load
+// add css for default grid appearing on load
 addCSS(sketchGrid, {
   gridTemplateRows: `repeat(${gridSizeInput.value}, 1fr)`,
   gridTemplateColumns: `repeat(${gridSizeInput.value}, 1fr)`,
 });
 
-// populate squares across the grid and draw borders
-function generateGrid(grid, square, size) {
-  const children = grid.children;
-  const borderStyle = "1px solid #555";
+// populate squares into the grid on page load
+generateGrid(sketchGrid, gridSize);
 
-  for (let i = 1; i <= size * size; i++) {
+
+function generateGrid(parent, size) {
+  const children = parent.children;
+  for (let i = 0; i < size * size; i++) {
     square = document.createElement("div");
-    grid.append(square);
-    square.style.cssText = `border-left: ${borderStyle}; border-top: ${borderStyle};`;
-  } // borders for every last element in a row
+    parent.append(square);
+  }
+  for (let i = 0; i < size * size; i++) {
+    children[i].style.cssText += `border-left: ${borderStyle}; border-top: ${borderStyle};`;
+  }
   for (let i = 1; i <= size * size; i += size) {
     children[i + size - 2].style.cssText += `border-right: ${borderStyle}`;
-  } // borders only for elements in the last row
+  }
   for (let i = children.length - 1; i >= children.length - size; i--) {
     children[i].style.cssText += `border-bottom: ${borderStyle}`;
   }
 }
 
-// function call so it populates squares into the grid on page load
-generateGrid(sketchGrid, gridSquare, defaultGridSize);
-
 // update grid size on input change
-gridSizeElement.addEventListener("change", (e) => {
-  const gridSizeValue = Number(e.target.value);
+gridSizeInput.addEventListener("change", updateGrid);
+
+function updateGrid (e) {
+  gridSize = Number(e.target.value);
+  e.target.setAttribute("value", `${gridSize}`)
 
   addCSS(sketchGrid, {
-    gridTemplateRows: `repeat(${gridSizeValue}, 1fr)`,
-    gridTemplateColumns: `repeat(${gridSizeValue}, 1fr)`,
+    gridTemplateRows: `repeat(${gridSize}, 1fr)`,
+    gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
   });
 
-  gridSizeLabel.innerText = `${gridSizeValue} x ${gridSizeValue}`;
+  gridSizeLabel.innerText = `${gridSize} x ${gridSize}`;
   sketchGrid.innerHTML = "";
-  generateGrid(sketchGrid, gridSquare, gridSizeValue);
-});
+  generateGrid(sketchGrid, gridSize);
+}
+
+
+toggleGridButton.addEventListener("click", toggleGrid);
+
+function toggleGrid(e) {
+  e.target.classList.toggle("toggled");
+  size = gridSize
+  const children = sketchGrid.children;
+  if (Array.from(e.target.classList).includes("toggled")) {
+    borderStyle = "none";
+  } else {
+    borderStyle = "1px solid #555";
+  }
+  for (let i = 0; i < size * size; i++) {
+    children[i].style.cssText += `border-left: ${borderStyle}; border-top: ${borderStyle};`;
+  }
+  for (let i = 1; i <= size * size; i += size) {
+    children[i + size - 2].style.cssText += `border-right: ${borderStyle}`;
+  }
+  for (let i = children.length - 1; i >= children.length - size; i--) {
+    children[i].style.cssText += `border-bottom: ${borderStyle}`;
+  }
+}
