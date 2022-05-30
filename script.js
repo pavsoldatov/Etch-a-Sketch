6fbdbcc4b5d911fd2dae824d499c9830b1e991e5
pavsoldatov <snowschoe@gmail.com>
2022-05-30 23:31:43 +0300

    Update toggle behavior to 'one click - one toggle'

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const sketchGrid = document.querySelector(".sketch-grid");
 const gridSizeInput = document.querySelector(".settings__grid-size-input");
-const gridSizeLabel = document.querySelector(".settings__grid-size-label");
-const gridSquare = document.createElement("div");
 const toggleGridButton = document.querySelector(
   ".settings__toggle-grid-button"
 );
 const clearGridBtn = document.querySelector(".settings__clear-grid-button");
-const rainbowModeBtn = document.querySelector(".settings__random-mode-button");
-const eraserBtn = document.querySelector(".settings__eraser-button");
-const colorFillBtn = document.querySelector(".settings__color-fill-button");
-let gridSize = Number(
-  document.querySelector(".settings__grid-size-input").value
-);
+const rainbowModeBtn = {
+  element: document.querySelector(".settings__random-mode-button"),
+  mode: "randomColor",
+};
+const eraserBtn = {
+  element: document.querySelector(".settings__eraser-button"),
+  mode: "eraser",
+};
+const colorFillBtn = {
+  element: document.querySelector(".settings__color-fill-button"),
+  mode: "colorFIll",
+};
+let gridSize = +document.querySelector(".settings__grid-size-input").value;
 let borderStyle = "1px solid #979797";
 let colorInput = document.querySelector(".settings__color");
 
@@ -19,37 +24,39 @@ function addCSS(element, style) {
   for (let property in style) element.style[property] = style[property];
 }
 
-// css for default grid appearing on load
+// utility functions
 addCSS(sketchGrid, {
   gridTemplateRows: `repeat(${gridSizeInput.value}, 1fr)`,
   gridTemplateColumns: `repeat(${gridSizeInput.value}, 1fr)`,
 });
 
-let buttons = [rainbowModeBtn, eraserBtn, colorFillBtn];
-let x = 0;
-let y = 0;
+function rgbToHex(string) {
+  let rgb = string.replace("rgb", "").replace("(", "").replace(")", "");
+  let rgbArr = rgb.split(", ");
+  r = (+rgbArr[0]).toString(16);
+  g = (+rgbArr[1]).toString(16);
+  b = (+rgbArr[2]).toString(16);
+
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+function hexToRGB(hex) {
+  let r = parseInt(hex[1] + hex[2], 16);
+  let g = parseInt(hex[3] + hex[4], 16);
+  let b = parseInt(hex[5] + hex[6], 16);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+// modes and corresponding behavior
 let isDrawing = false;
 let currentMode = "singleColor";
 document.body.onmousedown = () => (isDrawing = true);
 document.body.onmouseup = () => (isDrawing = false);
-rainbowModeBtn.onclick = (e) => {
-  rainbowModeBtn.classList.toggle("toggled");
-  Array.from(e.target.classList).includes("toggled")
-    ? (currentMode = "randomColor")
-    : (currentMode = "singleColor");
-};
-eraserBtn.onclick = (e) => {
-  eraserBtn.classList.toggle("toggled");
-  Array.from(e.target.classList).includes("toggled")
-    ? (currentMode = "eraser")
-    : (currentMode = "singleColor");
-};
-colorFillBtn.onclick = (e) => {
-  colorFillBtn.classList.toggle("toggled");
-  Array.from(e.target.classList).includes("toggled")
-    ? (currentMode = "colorFill")
-    : (currentMode = "singleColor");
-};
 
 function paint(e) {
   console.log(currentMode);
@@ -65,38 +72,47 @@ function paint(e) {
   } else if (currentMode === "eraser") {
     e.target.style.backgroundColor = "#fff";
   } else if (currentMode === "colorFill") {
-    let squares = e.target.parentElement.children;
-    let startEl = e.target;
-    let oldColor = e.target.style.backgroundColor;
-    let fillColor = hexToRGB(colorInput.value);
-    floodFIll(e.target, oldColor, fillColor);
   }
 }
 
-function floodFIll(square, oldColor, fillColor) {
-  hexToRGB(fillColor);
+// toggle behavior for buttons
+const toggleObjects = [rainbowModeBtn, eraserBtn, colorFillBtn];
+toggleObjects.forEach(
+  (toggleObject) =>
+    (toggleObject.element.onclick = (e) => {
+      // adding and removing toggles so that only one button can be toggled
+      const classes = Array.from(e.target.classList);
+      const isToggled = classes.includes("toggled");
+      toggleObjects.forEach((obj) => obj.element.classList.remove("toggled"));
+      e.target.classList.toggle("toggled", !isToggled);
 
-  if (oldColor !== fillColor) {
-    getCoordinates(square);
-    square.style.backgroundColor = fillColor;
-  }
-}
+      // setting the current mode
+      // note: classList arrays on top and below differ due to a removed class 'toggled'.
+      Array.from(e.target.classList).includes("toggled")
+        ? (currentMode = toggleObject.mode)
+        : (currentMode = "singleColor");
+      console.log(classes, Array.from(e.target.classList));
+    })
+);
 
+const generateRandomHex = () =>
+  `#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`;
+
+// getting x-y coords for the flood-fill
 function getCoordinates(target) {
   let gridArr = Array.from(target.parentElement.children);
   let width = parseInt(window.getComputedStyle(sketchGrid).width);
   let height = parseInt(window.getComputedStyle(sketchGrid).height);
 
   let x = Math.floor(
-    gridArr.indexOf(target) /
-    (width / (width / gridSizeInput.value))
+    gridArr.indexOf(target) / (width / (width / gridSizeInput.value))
   );
   let y =
     gridArr.indexOf(target) %
     Math.floor(height / (height / gridSizeInput.value));
-  console.log([x,y])
-
-  /*Here lies the body of a function I had spent days to make work properly.
+  console.log([x, y]);
+}
+/*  Here lies the body of a function I had spent days to make work properly.
     It died as it lived: (1)returning correct results, and (2)overflowing my stack.
     Days without sleep, spent in agony from infinite loops,
     eventually gave rise to this power-hungry piece of code...
@@ -138,29 +154,6 @@ function getCoordinates(target) {
     let x = rowsArr.findIndex((arr) => arr.includes(target));
     let y = colsArr.findIndex((arr) => arr.includes(target));
   */
-}
-
-function rgbToHex(string) {
-  let rgb = string.replace("rgb", "").replace("(", "").replace(")", "");
-  let rgbArr = rgb.split(", ");
-  r = (+rgbArr[0]).toString(16);
-  g = (+rgbArr[1]).toString(16);
-  b = (+rgbArr[2]).toString(16);
-
-  if (r.length == 1) r = "0" + r;
-  if (g.length == 1) g = "0" + g;
-  if (b.length == 1) b = "0" + b;
-
-  return "#" + r + g + b;
-}
-
-function hexToRGB(hex) {
-  let r = parseInt(hex[1] + hex[2], 16);
-  let g = parseInt(hex[3] + hex[4], 16);
-  let b = parseInt(hex[5] + hex[6], 16);
-
-  return `rgb(${r}, ${g}, ${b})`;
-}
 
 function populateSquares(parent, size) {
   for (let i = 0; i < size * size; i++) {
@@ -190,6 +183,8 @@ function drawGrid(array, size) {
 
 // update grid size on input change
 function updateGrid(e) {
+  const gridSizeLabel = document.querySelector(".settings__grid-size-label");
+
   gridSize = +e.target.value;
   e.target.setAttribute("value", `${gridSize}`);
 
@@ -231,6 +226,3 @@ function clearGrid() {
 }
 
 clearGridBtn.addEventListener("click", clearGrid);
-
-const generateRandomHex = () =>
-  `#${(Math.random() * 0xfffff * 1000000).toString(16).slice(0, 6)}`;
